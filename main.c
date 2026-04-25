af3b33c90de054ddd2d6f8670dec7f1527510fb3
matt-rog <91029371+matt-rog@users.noreply.github.com>
2026-04-24 21:30:14 -0500

    add parse dataset directory

--- main.c
+++ main.c
@@ -2,67 +2,90 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <wchar.h>
+#include <dirent.h>
+#include <string.h>
 
 struct Character {
-    char label;
+    wchar_t label[3];
     short imageWidth;
     short imageHeight;
     int charImage[];
 };
 
-#define MAX_CHARACTER_SAMPLES 100000
+#define MAX_CHARACTER_SAMPLES 10000
 
 int main() {
 
+    struct Character characters[MAX_CHARACTER_SAMPLES];
+
     setlocale(LC_ALL, "zh_CN.UTF-8");
     
-    FILE *fptr;
+    char dataset_path[500] = "train1.0/";
 
-    fptr = fopen("train1.0/001-f.gnt", "r");
-
-    if (fptr == NULL)
-    {
-        printf("GNT failed to open");
+    DIR *dir = opendir(dataset_path);
+    if (!dir) {
+        printf("Dataset directory failed to open");
         return 1;
     }
 
-    // Get file size
-    fseek(fptr, 0L, SEEK_END);
-    long int fileSize = ftell(fptr);
-    fseek(fptr, 0L, SEEK_SET);
-
     int i = 0;
-    while (ftell(fptr) < fileSize)
-    {
-        printf("\n%ld", ftell(fptr));
+    int maxW = -1;
+    int maxH = -1;
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {  
+        if (entry->d_type != DT_REG) 
+            continue;
+
+        FILE *fptr;
         
-        long sampleSize;
-        wchar_t charLabel[3];
-        short imageWidth;
-        short imageHeight;
-        
-        fread(&sampleSize, 4, 1, fptr);
-        fread(charLabel, 2, 1, fptr);
-        fread(&imageWidth, 2, 1, fptr);
-        fread(&imageHeight, 2, 1, fptr);
-        
-        printf("\n%ld", sampleSize);
-        printf("\n%ls", charLabel);
-        printf("\n%d", imageWidth);
-        printf("\n%d", imageHeight);
-        
-        int charImage[imageWidth * imageHeight];
-        fread(&charImage, imageWidth * imageHeight, 1, fptr);
-        
-        
-        FILE* imgFptr;
-        imgFptr = fopen("image.bin", "w");
-        
-        fwrite(charImage, sizeof(int), imageWidth * imageHeight, imgFptr);
-        fclose(imgFptr);
+        char file_path[500];
+        memcpy(file_path, dataset_path, strlen(dataset_path));
+        sprintf(file_path + strlen(entry->d_name), "%s", entry->d_name);
+        fptr = fopen(file_path, "r");
+    
+        if (fptr == NULL) {
+            printf("GNT failed to open");
+            return 1;
+        }
+    
+        // Get file size
+        fseek(fptr, 0L, SEEK_END);
+        long int fileSize = ftell(fptr);
+        fseek(fptr, 0L, SEEK_SET);
+    
+        while (ftell(fptr) < fileSize)
+        {
+            struct Character c;
+    
+            long sampleSize;
+            wchar_t charLabel[3];
+            short imageWidth;
+            short imageHeight;
+            
+            fread(&sampleSize, 4, 1, fptr);
+            fread(c.label, 2, 1, fptr);
+            fread(&c.imageWidth, 2, 1, fptr);
+            fread(&c.imageHeight, 2, 1, fptr);
+            
+            int charImage[c.imageWidth * c.imageHeight];
+            fread(&charImage, c.imageWidth * c.imageHeight, 1, fptr);
+            
+            if (c.imageWidth > maxW)
+                maxW = c.imageWidth;
+
+            if (c.imageHeight > maxH)
+                maxH = c.imageHeight;
+            
+            i++;
+        }
+        fclose(fptr);
     }
 
-    fclose(fptr);
+    printf("\n%d samples", i);
+    printf("\n%d maxW", maxW);
+    printf("\n%d maxH", maxH);
+
     return 0;
 
 }
